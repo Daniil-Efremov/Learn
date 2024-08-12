44f560c848f311d541459e3a002d766afcc6c21e
Daniil-Efremov <daniilefr2000@gmail.com>
2024-08-12 21:22:05 +0300

    promise

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    const prom = new Promise((resolve, reject)=>{
+        let x = 3;
+        if(x >= 5){
+            resolve("resolve: x>=5")
+        }else{
+            reject("reject: x<5")
+        }
+    }).then((msg) => {console.log(msg); 
+        return new Promise((resolve, reject) =>{
+        if( 1+1 == 3){
+            resolve("resolve: 1 + 1 = 2");
+        }else{
+            reject("error")
+        }
+            })
+    }).then((response)=> console.log(response))
+    .catch((error)=> console.log(error))
+    .catch((error)=>console.log(error));
+    .finally(console.log("finally"));
+
+});
